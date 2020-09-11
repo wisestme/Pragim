d116398bcee2b6fbc532f5b82cd011268a135873
wisestMe <wyhzest@gmail.com>
2020-09-11 03:15:35 +0100

    Add lists

--- Pragim/Services/ImplementLists.cs
+++ Pragim/Services/ImplementLists.cs
@@ -15,6 +15,24 @@ namespace Pragim.Services
             churchesList.Add(new Churches() { ID = 1, Name = "Latter Rain", Pastor = "Tunde Bakare", Location = "Oregun Lagos"});
             churchesList.Add(new Churches() { ID = 3, Name = "Daystar", Pastor = "Sam Adeyemi", Location = "Oregun, Lagos" });
         }
+
+        public void LagosMarketsList ()
+        {
+            List<LagosMarkets> lagosMarkets = new List<LagosMarkets>();
+
+            lagosMarkets.Add(new LagosMarkets() { ID = 1, Name = "Mile 12", Products = "Food Stuffs", Size = "50,000 shops" });
+            lagosMarkets.Add(new LagosMarkets() { ID = 2, Name = "Alaba", Products = "Electronics", Size = "100,000 shops" });
+            lagosMarkets.Add(new LagosMarkets() { ID = 3, Name = "Oshodi", Products = "Variety", Size = "200,000 shops"});
+        }
+
+        public void ListOfMalls()
+        {
+            List<LagosMalls> lagosMalls = new List<LagosMalls>();
+
+            lagosMalls.Add(new LagosMalls() { ID = 1, Name = "Ikeja City Mall", Capacity = "70,000 Shops", Location = "Alausa, Ikeja" });
+            lagosMalls.Add(new LagosMalls() { ID = 3, Name = "Maryland Mall", Capacity = "10,000 shops", Location = "Maryland" });
+        }
+          
     }
 
     class Churches
@@ -23,5 +41,21 @@ namespace Pragim.Services
         public string Name { get; set; }
         public string Pastor { get; set; }
         public string Location { get; set; }
+    }
+
+    class LagosMarkets
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Products { get; set; }
+        public string Size { get; set; }
+    }
+
+    class LagosMalls
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Capacity { get; set; }
     }
 }
