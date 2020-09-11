3df975c876bfc6f5437eed3ae01289565d7ab0b4
wisestMe <wyhzest@gmail.com>
2020-09-11 02:40:45 +0100

    add churches list

---
+++ Pragim/Services/ImplementLists.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pragim.Services
+{
+    class ImplementLists
+    {
+        public void ListOfChurches()
+        {
+            List<Churches> churchesList = new List<Churches>();
+            churchesList.Add(new Churches() { ID = 0, Name = "Household", Pastor = "Chris Okotie", Location = "Oregun, Lagos" });
+            churchesList.Add(new Churches() { ID = 1, Name = "Latter Rain", Pastor = "Tunde Bakare", Location = "Oregun Lagos"});
+            churchesList.Add(new Churches() { ID = 3, Name = "Daystar", Pastor = "Sam Adeyemi", Location = "Oregun, Lagos" });
+        }
+    }
+
+    class Churches
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Pastor { get; set; }
+        public string Location { get; set; }
+    }
+}
