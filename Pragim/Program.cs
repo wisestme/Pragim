4f78563c0b95e2465451f3169e0cdf4d00d2215e
wisestMe <wyhzest@gmail.com>
2020-10-30 17:16:24 +0100

    write to console

--- Pragim/Program.cs
+++ Pragim/Program.cs
@@ -4,118 +4,102 @@ using Pragim.Services;
 
 namespace Pragim
 {
-    public class Employee
-    {
-        public string FirstName;
-        public string LastName;
-        public void PrintFullName()
-        {
-            Console.WriteLine(FirstName + " " + LastName);
-        }
-    }
-    public class PartTimeEmployee : Employee
-    {
-        public new void PrintFullName()
-        {
-            
-            Console.WriteLine(FirstName + " " + LastName + " - PT");
-        }
-    }
-
-    public class FullTimeEmployee : Employee
-    {
-
-    }
+    
     public class Program
     {
-        
-
+        public static void NotMain()
+        {
+            Console.WriteLine("I am not the main function");
+        }
         public static void Main()
         {
-            ImplementLists implementLists = new ImplementLists();
-            implementLists.LagosMarketsList();
+            
+            Console.WriteLine("Hello wisest... welcome to c# training");
+            NotMain();
+            //ImplementLists implementLists = new ImplementLists();
+            //implementLists.LagosMarketsList();
 
-            WorkersPromotion workersPromotion = new WorkersPromotion();
+            //WorkersPromotion workersPromotion = new WorkersPromotion();
 
-            PromotionDelegate agePromotion = new PromotionDelegate(workersPromotion.PromoteByAge);
-            agePromotion(40);
+            //PromotionDelegate agePromotion = new PromotionDelegate(workersPromotion.PromoteByAge);
+            //agePromotion(40);
 
-            PromotionDelegate experiencePromotion = new PromotionDelegate(workersPromotion.PromoteByExperience);
-            experiencePromotion(7);
+            //PromotionDelegate experiencePromotion = new PromotionDelegate(workersPromotion.PromoteByExperience);
+            //experiencePromotion(7);
 
-            EmployeePromoter promoter = new EmployeePromoter();
-            promoter.AddEmployee();
+            //EmployeePromoter promoter = new EmployeePromoter();
+            //promoter.AddEmployee();
             
 
-            Calculator adder = new Calculator(CalculatorDelegate.add);
-            Console.WriteLine(adder(5));
+            //Calculator adder = new Calculator(CalculatorDelegate.add);
+            //Console.WriteLine(adder(5));
 
-            Calculator multiplier = new Calculator(CalculatorDelegate.multiply);
-            Console.WriteLine(multiplier(2));
+            //Calculator multiplier = new Calculator(CalculatorDelegate.multiply);
+            //Console.WriteLine(multiplier(2));
 
-            Calculator getNum = new Calculator(CalculatorDelegate.getNumber);
-            Console.WriteLine(getNum(2));
+            //Calculator getNum = new Calculator(CalculatorDelegate.getNumber);
+            //Console.WriteLine(getNum(2));
 
-            //GreetDelegate del = new GreetDelegate(ImplementDelegate.Greet);
-            //del("How are you doing");
+            ////GreetDelegate del = new GreetDelegate(ImplementDelegate.Greet);
+            ////del("How are you doing");
 
-            //GreetDelegate warner = new GreetDelegate(ImplementDelegate.Warn);
-            //warner("Keep out of reach of everybody");
+            ////GreetDelegate warner = new GreetDelegate(ImplementDelegate.Warn);
+            ////warner("Keep out of reach of everybody");
 
-            ImplementDelegate implementDelegate = new ImplementDelegate();
+            //ImplementDelegate implementDelegate = new ImplementDelegate();
             
-            TimelyGreeting greetingMorning = new TimelyGreeting(implementDelegate.GreetMorning);
-            greetingMorning("Morning");
+            //TimelyGreeting greetingMorning = new TimelyGreeting(implementDelegate.GreetMorning);
+            //greetingMorning("Morning");
 
-            TimelyGreeting greetAfternoon = new TimelyGreeting(implementDelegate.GreetAfternoon);
-            greetAfternoon("Afternoon");
+            //TimelyGreeting greetAfternoon = new TimelyGreeting(implementDelegate.GreetAfternoon);
+            //greetAfternoon("Afternoon");
 
-            TimelyGreeting greetEvening = new TimelyGreeting(implementDelegate.GreetEvening);
-            greetEvening("Evening");
+            //TimelyGreeting greetEvening = new TimelyGreeting(implementDelegate.GreetEvening);
+            //greetEvening("Evening");
 
-            AdditionCalculator addition = new AdditionCalculator();
-            addition.Integrate();
-            CustomerAbstract differentCustomer = new CustomerAbstract();
-            differentCustomer.Print();
+            //AdditionCalculator addition = new AdditionCalculator();
+            //addition.Integrate();
+            //CustomerAbstract differentCustomer = new CustomerAbstract();
+            //differentCustomer.Print();
 
-            Customer customer1 = new Customer();
-            customer1.Print();
-            ((IAnotherInterface)customer1).Print();
-            customer1.IAnotherInterfaceMethod();
+            //Customer customer1 = new Customer();
+            //customer1.Print();
+            //((IAnotherInterface)customer1).Print();
+            //customer1.IAnotherInterfaceMethod();
 
-            PolyEmployee[] employees = new PolyEmployee[4];
+            //PolyEmployee[] employees = new PolyEmployee[4];
 
-            employees[0] = new PolyEmployee();
-            employees[1] = new PolyPartTimeEmployee();
-            employees[2] = new PolyFullTimeEmployee();
-            employees[3] = new PolyTemporaryEmployee();
+            //employees[0] = new PolyEmployee();
+            //employees[1] = new PolyPartTimeEmployee();
+            //employees[2] = new PolyFullTimeEmployee();
+            //employees[3] = new PolyTemporaryEmployee();
 
-            foreach (PolyEmployee e in employees)
-            {
-                e.PrintFullName();
-            }
+            //foreach (PolyEmployee e in employees)
+            //{
+            //    e.PrintFullName();
+            //}
 
-            FullTimeEmployee FTE = new FullTimeEmployee();
-            FTE.FirstName = "Chijioke";
-            FTE.LastName = "Nwagwu";
-            FTE.PrintFullName();
+            //FullTimeEmployee FTE = new FullTimeEmployee();
+            //FTE.FirstName = "Chijioke";
+            //FTE.LastName = "Nwagwu";
+            //FTE.PrintFullName();
 
-            Employee PTE = new PartTimeEmployee();
-            PTE.FirstName = "Gloria";
-            PTE.LastName = "Vincent";
-            PTE.PrintFullName();
+            //Employee PTE = new PartTimeEmployee();
+            //PTE.FirstName = "Gloria";
+            //PTE.LastName = "Vincent";
+            //PTE.PrintFullName();
 
-            Pragim.Services.Car VICar = new Pragim.Services.Car();
-            VICar.Type = "Jeep";
-            VICar.EngineNumber = "BE5050";
-            VICar.Color = "Red";
-            VICar.VehicleDetails();
+            //Pragim.Services.Car VICar = new Pragim.Services.Car();
+            //VICar.Type = "Jeep";
+            //VICar.EngineNumber = "BE5050";
+            //VICar.Color = "Red";
+            //VICar.VehicleDetails();
 
-            VehicleInheritance VIBike = new VehicleInheritance();
-            VIBike.Type = "PowerBike";
-            VIBike.EngineNumber = "PB002110";
-            VIBike.Color = "Blue";
-            VIBike.VehicleDetails();
+            //VehicleInheritance VIBike = new VehicleInheritance();
+            //VIBike.Type = "PowerBike";
+            //VIBike.EngineNumber = "PB002110";
+            //VIBike.Color = "Blue";
+            //VIBike.VehicleDetails();
         }
     }
 }
